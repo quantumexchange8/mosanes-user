843f0242471c2217963d007a77064427750f16ce
choo <choohuanlong@gmail.com>
2024-07-29 13:25:28 +0800

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\MediaLibrary\HasMedia;
@@ -78,5 +80,20 @@ class User extends Authenticatable implements HasMedia
     public function groupHasUser(): HasOne
     {
         return $this->hasOne(GroupHasUser::class, 'user_id');
+    }
+
+    public function directChildren(): HasMany
+    {
+        return $this->hasMany(User::class, 'upline_id', 'id');
+    }
+
+    public function upline(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'upline_id', 'id');
+    }
+
+    public function paymentAccounts(): HasMany
+    {
+        return $this->hasMany(PaymentAccount::class, 'user_id', 'id');
     }
 }
