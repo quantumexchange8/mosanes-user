d9a8113efa954dc87c017ba58778f6500131dbb6
Wayne Lai <waynelai06@gmail.com>
2024-08-06 18:12:20 +0800

    fix: delete account

--- resources/js/Pages/TradingAccount/Partials/Action.vue
+++ resources/js/Pages/TradingAccount/Partials/Action.vue
@@ -1,7 +1,6 @@
 <script setup>
-import {ref, watchEffect, onMounted, h, computed} from 'vue';
+import {ref, h, computed} from 'vue';
 import { usePage, useForm } from "@inertiajs/vue3";
-import axios from 'axios';
 import Dialog from 'primevue/dialog';
 import Button from "@/Components/Button.vue";
 import {
@@ -12,16 +11,10 @@ import {
     IconDatabaseMinus,
     IconTrash
 } from '@tabler/icons-vue';
-import OverlayPanel from "primevue/overlaypanel";
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import InputText from 'primevue/inputtext';
-import IconField from 'primevue/iconfield';
-import Dropdown from "primevue/dropdown";
 import toast from '@/Composables/toast';
 import AccountReport from '@/Pages/TradingAccount/Partials/AccountReport.vue';
 import { useConfirm } from 'primevue/useconfirm';
-import { trans, wTrans } from "laravel-vue-i18n";
+import { trans } from "laravel-vue-i18n";
 import TieredMenu from "primevue/tieredmenu";
 import AccountWithdrawal from "@/Pages/TradingAccount/Partials/AccountWithdrawal.vue";
 import ChangeLeverage from "@/Pages/TradingAccount/Partials/ChangeLeverage.vue";
@@ -32,98 +25,87 @@ const props = defineProps({
     leverages: Array,
     walletOptions: Array,
 });
-const op = ref(null);
-const showWithdrawalDialog = ref(false);
-const showLeverageDialog = ref(false);
-const showAccountReportDialog = ref(false);
-const showTnCDialog = ref(false);
-const leverages = ref(props.leverages);
-const walletOptions = ref(props.walletOptions);
 
 const paymentAccounts = usePage().props.auth.payment_account;
+const menu = ref();
+const visible = ref(false);
+const dialogType = ref('');
+
+const items = ref([
+    {
+        label: 'withdrawal',
+        icon: h(IconCreditCardPay),
+        command: () => {
+            if (paymentAccounts.length === 0) {
+                requireAccountConfirmation('crypto');
+            } else {
+                visible.value = true;
+                dialogType.value = 'withdrawal';
+            }
+        },
+    },
+    {
+        label: 'change_leverage',
+        icon: h(IconScale),
+        command: () => {
+            if (props.account.account_type_leverage === 0) {
+                visible.value = true;
+                dialogType.value = 'change_leverage';
+            } else {
+                toast.add({
+                    title: trans('public.toast_leverage_change_warning'),
+                    type: 'warning',
+                });
+            }
+        },
+        account_type: 'Standard Account'
+    },
+    {
+        label: 'revoke_pamm',
+        icon: h(IconDatabaseMinus),
+        command: () => {
+            visible.value = true;
+            dialogType.value = 'revoke_pamm';
+        },
+        account_type: 'Premium Account'
+    },
+    {
+        label: 'account_report',
+        icon: h(IconHistory),
+        command: () => {
+            visible.value = true;
+            dialogType.value = 'account_report';
+        },
+    },
+    {
+        separator: true,
+    },
+    {
+        label: 'delete_account',
+        icon: h(IconTrash),
+        command: () => {
+            requireAccountConfirmation('live');
+        },
+    },
+]);
+
+const filteredItems = computed(() => {
+    return items.value.filter(item => {
+        if (item.account_type) {
+            return item.account_type === props.account.account_type;
+        }
+        return true;
+    });
+});
 
 const toggle = (event) => {
     menu.value.toggle(event);
 };
 
-const openDialog = (dialogRef, formRef = null) => {
-    if (formRef) formRef.reset();
-    if (dialogRef === 'withdrawal') {
-        // Check if paymentAccounts is empty
-        if (paymentAccounts.length === 0) {
-            requireAccountConfirmation('crypto');
-        } else {
-            showWithdrawalDialog.value = true;
-        }
-    } else if (dialogRef === 'leverage') {
-        if (props.account.leverage !== 0) {
-            showLeverageDialog.value = true;
-        } else {
-            toast.add({
-                title: trans('public.toast_leverage_change_warning'),
-                type: 'warning',
-            });
-        }
-    } else if (dialogRef === 'accountReport') {
-        showAccountReportDialog.value = true;
-    } else if (dialogRef === 'tnc') {
-        showTnCDialog.value = true;
-    }
-};
-
-const closeDialog = (dialogName, formRef = null) => {
-    if (formRef) formRef.reset();
-    if (dialogName === 'withdrawal') {
-        showWithdrawalDialog.value = false;
-    } else if (dialogName === 'leverage') {
-        showLeverageDialog.value = false;
-    } else if (dialogName === 'accountReport') {
-        showAccountReportDialog.value = false;
-    } else if (dialogName === 'tnc') {
-        showTnCDialog.value = false;
-    }
-};
-
-const withdrawalForm = useForm({
-    account_id: props.account.id,
-    amount: '',
-    receiving_wallet: '',
-});
-
-const leverageForm = useForm({
-    account_id: props.account.id,
-    leverage: '',
-});
-
 const form = useForm({
     account_id: props.account.id,
     type: '',
 });
-
-
-const submitWithdrawal = () => {
-    withdrawalForm.receiving_wallet = paymentAccounts[0].account_no
-    withdrawalForm.post(route('account.withdrawal_from_account'), {
-        onSuccess: () => {
-            // Close the dialog and show the withdrawal request dialog
-            closeDialog('withdrawal', withdrawalForm);
-        },
-        onError: (errors) => {
-            console.error('Error processing withdrawal:', errors);
-        }
-    });
-};
-
-const submitChangeLeverage = () => {
-    leverageForm.post(route('account.change_leverage'), {
-        onSuccess: () => {
-            closeDialog('leverage', leverageForm)
-        },
-        onError: (errors) => {
-            console.error('Error processing leverage change:', errors);
-        }
-    });
-};
 
 const confirm = useConfirm();
 const requireAccountConfirmation = (accountType) => {
@@ -195,85 +177,6 @@ const requireAccountConfirmation = (accountType) => {
         accept: action
     });
 };
-
-const toggleFullAmount = () => {
-    if (withdrawalForm.amount) {
-        withdrawalForm.amount = '';
-    } else {
-        withdrawalForm.amount = props.account.balance;
-    }
-};
-
-
-// new
-const menu = ref();
-const visible = ref(false);
-const dialogType = ref('');
-
-const items = ref([
-    {
-        label: trans('public.withdrawal'),
-        icon: h(IconCreditCardPay),
-        command: () => {
-            visible.value = true;
-            dialogType.value = 'withdrawal';
-        },
-    },
-    {
-        label: trans('public.change_leverage'),
-        icon: h(IconScale),
-        command: () => {
-            if (props.account.account_type_leverage === 0) {
-                visible.value = true;
-                dialogType.value = 'change_leverage';
-            } else {
-                toast.add({
-                    title: trans('public.toast_leverage_change_warning'),
-                    type: 'warning',
-                });
-            }
-        },
-        account_type: 'Standard Account'
-    },
-    {
-        label: trans('public.revoke_pamm'),
-        icon: h(IconDatabaseMinus),
-        command: () => {
-            visible.value = true;
-            dialogType.value = 'revoke_pamm';
-        },
-        account_type: 'Premium Account'
-    },
-    {
-        label: trans('public.account_report'),
-        icon: h(IconHistory),
-        command: () => {
-            visible.value = true;
-            dialogType.value = 'account_report';
-        },
-    },
-    {
-        separator: true,
-    },
-    {
-        label: trans('public.delete_account'),
-        icon: h(IconTrash),
-        command: () => {
-            visible.value = true;
-            dialogType.value = 'delete_account';
-        },
-        action: 'delete_account'
-    },
-]);
-
-const filteredItems = computed(() => {
-    return items.value.filter(item => {
-        if (item.account_type) {
-            return item.account_type === props.account.account_type;
-        }
-        return true;
-    });
-});
 </script>
 
 <template>
@@ -310,8 +213,8 @@ const filteredItems = computed(() => {
                 class="flex items-center gap-3 self-stretch"
                 v-bind="props.action"
             >
-                <component :is="item.icon" size="20" stroke-width="1.25" :color="item.action === 'delete_account' ? '#F04438' : '#667085'" />
-                <span class="font-medium" :class="{'text-error-500': item.action === 'delete_account'}">{{ item.label }}</span>
+                <component :is="item.icon" size="20" stroke-width="1.25" :color="item.label === 'delete_account' ? '#F04438' : '#667085'" />
+                <span class="font-medium" :class="{'text-error-500': item.label === 'delete_account'}">{{ $t(`public.${item.label}`) }}</span>
             </div>
         </template>
     </TieredMenu>
@@ -344,164 +247,4 @@ const filteredItems = computed(() => {
             />
         </template>
     </Dialog>
-
-    <OverlayPanel ref="op">
-        <div class="w-60 py-2 flex flex-col items-center">
-            <div class="p-3 flex items-center gap-3 self-stretch hover:bg-gray-100 hover:cursor-pointer" @click="openDialog('withdrawal', withdrawalForm)">
-                <IconCreditCardPay size="20" stroke-width="1.25" color="#667085" />
-                <div class="text-gray-950 text-sm font-medium">
-                    {{ $t('public.withdrawal') }}
-                </div>
-            </div>
-            <div v-if="props.type == 'individual'" class="p-3 flex items-center gap-3 self-stretch hover:bg-gray-100 hover:cursor-pointer" @click="openDialog('leverage', leverageForm)">
-                <IconScale size="20" stroke-width="1.25" color="#667085" />
-                <div class="text-gray-950 text-sm font-medium">
-                    {{ $t('public.change_leverage') }}
-                </div>
-            </div>
-            <div v-if="props.type == 'manage'" class="p-3 flex items-center gap-3 self-stretch hover:bg-gray-100 hover:cursor-pointer" @click="requireAccountConfirmation('revoke')">
-                <IconDatabaseMinus size="20" stroke-width="1.25" color="#667085" />
-                <div class="text-gray-950 text-sm font-medium">
-                    {{ $t('public.revoke_pamm') }}
-                </div>
-            </div>
-            <div class="p-3 flex items-center gap-3 self-stretch hover:bg-gray-100 hover:cursor-pointer" @click="openDialog('accountReport')">
-                <IconHistory size="20" stroke-width="1.25" color="#667085" />
-                <div class="text-gray-950 text-sm font-medium">
-                    {{ $t('public.account_report') }}
-                </div>
-            </div>
-            <div class="h-1 self-stretch bg-gray-200"></div>
-            <div class="p-3 flex items-center gap-3 self-stretch hover:bg-gray-100 hover:cursor-pointer" @click="requireAccountConfirmation('live')">
-                <IconTrash size="20" stroke-width="1.25" color="#F04438" />
-                <div class="text-error-500 text-sm font-medium">
-                    {{ $t('public.delete_account') }}
-                </div>
-            </div>
-        </div>
-    </OverlayPanel>
-
-    <!-- Withdrawal Dialog -->
-    <Dialog v-model:visible="showWithdrawalDialog" :header="$t('public.withdrawal')" modal class="dialog-xs sm:dialog-sm">
-        <form @submit.prevent="submitWithdrawal">
-            <div class="flex flex-col items-center gap-8 self-stretch sm:gap-10">
-                <div class="flex flex-col items-center gap-5 self-stretch">
-                    <div class="flex flex-col justify-center items-center py-4 px-8 gap-2 self-stretch bg-gray-200">
-                        <span class="w-full text-gray-500 text-center text-xs font-medium">#{{ props.account.meta_login}} - {{ $t('public.current_account_balance') }}</span>
-                        <span class="w-full text-gray-950 text-center text-xl font-semibold">$ {{ props.account.balance}}</span>
-                    </div>
-                    <div class="flex flex-col items-start gap-1 self-stretch">
-                        <InputLabel for="amount" :value="$t('public.amount')" />
-                        <div class="relative w-full">
-                            <IconField iconPosition="left" class="w-full">
-                                    <div class="text-gray-950 text-sm">$</div>
-                                    <InputText
-                                        id="amount"
-                                        type="number"
-                                        class="block w-full"
-                                        v-model="withdrawalForm.amount"
-                                        :placeholder="$t('public.amount_placeholder')"
-                                        :invalid="!!withdrawalForm.errors.amount"
-                                    />
-                            </IconField>
-                            <span
-                                class="absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer font-semibold"
-                                :class="{
-                                    'text-primary-500': !withdrawalForm.amount,
-                                    'text-error-500': withdrawalForm.amount,
-                                }"
-                                @click="toggleFullAmount"
-                            >
-                                {{ withdrawalForm.amount ? $t('public.clear') : $t('public.full_amount') }}
-                            </span>
-                        </div>
-                        <span class="self-stretch text-gray-500 text-xs">{{ $t('public.minimum_amount') }}: ${{ $t('public.minimum_amount_placeholder') }}</span>
-                        <InputError :message="withdrawalForm.errors.amount" />
-                    </div>
-                    <div class="flex flex-col items-start gap-1 self-stretch">
-                        <InputLabel for="receiving_wallet" :value="$t('public.receiving_wallet')" />
-                        <Dropdown
-                            v-model="withdrawalForm.receiving_wallet"
-                            :options="walletOptions"
-                            optionLabel="name"
-                            optionValue="value"
-                            :placeholder="$t('public.receiving_wallet_placeholder')"
-                            class="w-full"
-                            scroll-height="236px"
-                            :invalid="!!withdrawalForm.errors.receiving_wallet"
-                        />
-                        <span class="self-stretch text-gray-500 text-xs">{{ withdrawalForm.receiving_wallet }}</span>
-                        <InputError :message="withdrawalForm.errors.receiving_wallet" />
-                    </div>
-                </div>
-                <div class="self-stretch">
-                    <span class="text-gray-500 text-xs">{{ $t('public.agreement_text') }} <span class="text-primary-500 text-xs font-medium" @click="openDialog('tnc')">{{ $t('public.tnc') }}</span>.</span>
-                </div>
-            </div>
-            <div class="flex justify-end items-center pt-5 gap-4 self-stretch sm:pt-7">
-                <Button type="button" variant="gray-tonal" class="w-full sm:w-[120px]" @click.prevent="closeDialog('withdrawal', withdrawalForm)">{{ $t('public.cancel') }}</Button>
-                <Button variant="primary-flat" class="w-full sm:w-[120px]" @click.prevent="submitWithdrawal">{{ $t('public.confirm') }}</Button>
-            </div>
-        </form>
-    </Dialog>
-
-    <!-- Change Leverage Dialog -->
-    <Dialog v-model:visible="showLeverageDialog" :header="$t('public.change_leverage')" modal class="dialog-xs sm:dialog-sm">
-        <form @submit.prevent="submitChangeLeverage">
-            <div class="flex flex-col items-center gap-8 self-stretch sm:gap-10">
-                <div class="flex flex-col items-center gap-5 self-stretch">
-                    <div class="flex flex-col justify-center items-center py-4 px-8 gap-2 self-stretch bg-gray-200">
-                        <span class="w-full text-gray-500 text-center text-xs font-medium">#{{ props.account.meta_login}} - {{ $t('public.current_account_balance') }}</span>
-                        <span class="w-full text-gray-950 text-center text-xl font-semibold">$ {{ props.account.balance}}</span>
-                    </div>
-                    <div class="flex flex-col items-start gap-1 self-stretch">
-                        <InputLabel for="leverage" :value="$t('public.leverages')" />
-                        <Dropdown
-                            v-model="leverageForm.leverage"
-                            :options="leverages"
-                            optionLabel="name"
-                            optionValue="value"
-                            :placeholder="$t('public.leverages_placeholder')"
-                            class="w-full"
-                            scroll-height="236px"
-                            :invalid="leverageForm.errors.leverage"
-                        />
-                        <InputError :message="leverageForm.errors.leverage" />
-                    </div>
-                </div>
-                <div class="self-stretch">
-                    <span class="text-gray-500 text-xs">
-                        {{ $t('public.agreement_text') }}
-                        <span class="text-primary-500 text-xs font-medium">
-                            {{ $t('public.trading_account_agreement') }}
-                        </span>
-                        .
-                    </span>
-                </div>
-            </div>
-            <div class="flex justify-end items-center pt-5 gap-4 self-stretch sm:pt-7">
-                <Button type="button" variant="gray-tonal" class="w-full sm:w-[120px]" @click.prevent="closeDialog('leverage', leverageForm)">{{ $t('public.cancel') }}</Button>
-                <Button variant="primary-flat" class="w-full sm:w-[120px]" @click.prevent="submitChangeLeverage">{{ $t('public.confirm') }}</Button>
-            </div>
-        </form>
-    </Dialog>
-
-    <!-- Account Report Dialog -->
-    <Dialog v-model:visible="showAccountReportDialog" :header="`${$t('public.account_report')} #${props.account.meta_login}`" modal class="dialog-xs sm:dialog-md">
-        <div class="flex flex-col items-center gap-3 self-stretch sm:gap-5">
-            <div class="flex flex-col items-center self-stretch">
-                <AccountReport :account="props.account" />
-            </div>
-        </div>
-    </Dialog>
-
-    <Dialog v-model:visible="showTnCDialog" modal :header="$t('public.tnc')" class="dialog-xs md:dialog-lg">
-        <div class="flex flex-col items-center pb-4 gap-8 self-stretch md:pb-7 md:gap-10">
-            <span class="self-stretch text-gray-950 text-sm"></span>
-            <div class="flex flex-col items-center gap-5 self-stretch">
-
-            </div>
-        </div>
-    </Dialog>
-
 </template>
