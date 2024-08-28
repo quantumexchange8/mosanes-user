5c0561d5137e2b9fa3bfd103c06bf03ba1993713
Wayne Lai <waynelai06@gmail.com>
2024-08-28 17:31:39 +0800

    fix: target achievement loading

--- resources/js/Pages/Billboard/Billboard.vue
+++ resources/js/Pages/Billboard/Billboard.vue
@@ -60,8 +60,47 @@ getResults();
             </div>
 
             <div v-else class="w-full">
-                <div v-if="isLoading">
+                <div v-if="isLoading" class="grid grid-cols-1 md:grid-cols-2 gap-5 self-stretch">
+                    <div
+                        class="rounded-2xl shadow-toast bg-white"
+                    >
+                        <div class="py-5 px-4 md:p-6 flex flex-col items-center gap-3 md:gap-4 self-stretch">
+                            <div class="flex justify-between items-center self-stretch w-full">
+                                <StatusBadge value="info" />
+                                <span class="text-xxs text-gray-700 font-medium">#{{ $t('public.achieve_target_to_unlock_bonus', {'threshold': '70'}) }}</span>
+                            </div>
 
+                            <div class="py-2 flex gap-2 justify-center items-center self-stretch">
+                                <div class="flex flex-col gap-1 self-stretch w-full animate-pulse">
+                                    <div class="h-2.5 bg-gray-200 rounded-full w-40 my-1"></div>
+                                    <div class="h-2 bg-gray-200 rounded-full w-36 my-1"></div>
+                                </div>
+                                <div class="flex flex-col gap-1 self-stretch items-end w-full animate-pulse">
+                                    <div class="h-2.5 bg-primary-400 rounded-full w-40 my-1"></div>
+                                    <div class="h-2 bg-gray-200 rounded-full w-36 my-1"></div>
+                                </div>
+                            </div>
+
+                            <div class="flex justify-center items-center self-stretch w-full">
+                                <MeterGauge :percentage="0" />
+                            </div>
+
+                            <div class="flex pt-4 items-center justify-center self-stretch w-full animate-pulse">
+                                <div v-tooltip.bottom="$t('public.sales_category')" class="flex flex-col gap-3 items-center self-stretch w-full">
+                                    <IconChartArcs size="20" color="#98A2B3" stroke-width="1.25" />
+                                    <div class="h-2.5 bg-gray-200 rounded-full w-40 my-1"></div>
+                                </div>
+                                <div v-tooltip.bottom="$t('public.bonus_rate')" class="flex flex-col gap-3 items-center self-stretch w-full">
+                                    <IconCirclePercentage size="20" color="#98A2B3" stroke-width="1.25" />
+                                    <div class="h-2.5 bg-gray-200 rounded-full w-40 my-1"></div>
+                                </div>
+                                <div v-tooltip.bottom="$t('public.next_calculation_date')" class="flex flex-col gap-3 items-center self-stretch w-full">
+                                    <IconCalendarStats size="20" color="#98A2B3" stroke-width="1.25" />
+                                    <div class="h-2.5 bg-gray-200 rounded-full w-40 my-1"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div
@@ -72,7 +111,7 @@ getResults();
                         v-for="achievement in targetAchievements"
                         class="rounded-2xl shadow-toast bg-white"
                     >
-                        <div class="py-5 px-4 md:p-6 flex flex-col items-center gap-3 md:gap-4 self-stretch ">
+                        <div class="py-5 px-4 md:p-6 flex flex-col items-center gap-3 md:gap-4 self-stretch">
                             <div class="flex justify-between items-center self-stretch w-full">
                                 <StatusBadge :value="achievement.bonus_badge">
                                     {{ $t(`public.${achievement.sales_calculation_mode}`) }}
