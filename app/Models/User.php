780d6895fb711234a6508b48e779eb3f4a5e6e65
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2024-09-04 20:48:20 +0800

    fix: user soft deletes

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
@@ -17,7 +18,7 @@ use Spatie\MediaLibrary\InteractsWithMedia;
 
 class User extends Authenticatable implements HasMedia, MustVerifyEmail
 {
-    use HasFactory, Notifiable, InteractsWithMedia, LogsActivity;
+    use SoftDeletes, HasFactory, Notifiable, InteractsWithMedia, LogsActivity;
 
     /**
      * The attributes that are mass assignable.
